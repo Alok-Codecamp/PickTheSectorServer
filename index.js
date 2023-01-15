4daaa6527f80f4f5feeda0f7d9d351a6d302b0da
Alok Kumar <alok61.bd@gmail.com>
2023-01-15 22:26:41 +0600

    app.use added

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mrwnd.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
+app.use('/', (req, res) => {
+    return res.sendFile(path.resolve(__dirname, '../../dist', 'index.html'));
+  });
 async function run(){
     try{
         await client.connect();
@@ -48,7 +50,9 @@ run().catch(console.dir);
 app.get('/',(req,res)=>{
     res.send('hello world!')
 })
-
+app.use('/', (req, res) => {
+    return res.sendFile(path.resolve(__dirname, '../../dist', 'index.html'));
+  });
 app.listen(port,()=>{
     console.log(`example app listesing at port: ${port}`)
 })
